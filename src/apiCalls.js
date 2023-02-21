1abfd5ab4ef47ef3cff8e0ead3efbb69c02188e5
Blanche Haddad <blancherachel@gmail.com>
2023-02-21 11:42:43 -0700

    feat: Refactor post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,4 +9,21 @@ const fetchReservations = () => {
     })
 }
 
-export default fetchReservations;
+const postReservation = (res) => {
+  return fetch("http://localhost:3001/api/v1/reservations", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(res)
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Sorry, couldn't post")
+      } else {
+        return res.json();
+      }
+    })
+}
+
+export { fetchReservations, postReservation }
